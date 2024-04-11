faeb932953f38d1df9381b658ae57e050d196473
ClaytonDewey <clay@dryadmedia.com>
2024-04-11 16:47:09 -0500

    Add Icons and margin

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 import Button from './Button';
 
 const App = () => {
@@ -5,16 +6,21 @@ const App = () => {
     <div>
       <div>
         <Button success rounded outline>
+          <GoBell />
           Click me!!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy Now!
         </Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
