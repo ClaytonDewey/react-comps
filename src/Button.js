1e0c091c8b73feef1a1cd4a0a50447ffa4f63160
ClaytonDewey <clay@dryadmedia.com>
2024-04-11 15:27:04 -0500

    Add classes & install dependencies

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,5 @@
+import className from 'classnames';
+
 const Button = ({
   children,
   primary,
@@ -8,7 +10,22 @@ const Button = ({
   outline,
   rounded,
 }) => {
-  return <button>{children}</button>;
+  const classes = className('px-3 mb-4 py-1.5 border', {
+    'border-blue-600 bg-blue-500 text-white': primary,
+    'border-gray-900 bg-gray-800 text-white': secondary,
+    'border-green-600 bg-green-500 text-white': success,
+    'border-yellow-500 bg-yellow-400 text-white': warning,
+    'border-red-600 bg-red-500 text-white': danger,
+    'rounded-full': rounded,
+    'bg-white': outline,
+    'text-blue-500': outline && primary,
+    'text-gray-900': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-400': outline && warning,
+    'text-red-500': outline && danger,
+  });
+
+  return <button className={classes}>{children}</button>;
 };
 
 Button.propTypes = {
