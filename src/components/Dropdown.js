af3587efc2301bfbfdd06d9603969f3e64b52856
ClaytonDewey <clay@dryadmedia.com>
2024-04-19 12:48:27 -0500

    Create a dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,20 +1,43 @@
 import { useState } from 'react';
+import { GoChevronDown } from 'react-icons/go';
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleOptionClick = (option) => {
+    // CLOSE DROPDOWN
+    setIsOpen(false);
+    // WHAT OPTION DID THE USER CLICK ON???
+    onChange(option);
+  };
+
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>{option.label}</div>;
+    return (
+      <div
+        className='hover:bg-sky-100 rounded cursor-pointer p-1'
+        onClick={() => handleOptionClick(option)}
+        key={option.value}>
+        {option.label}
+      </div>
+    );
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>Select...</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className='w-48 relative'>
+      <div
+        className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full'
+        onClick={handleClick}>
+        {value?.label || 'Select...'} <GoChevronDown className='text-lg' />
+      </div>
+      {isOpen && (
+        <div className='absolute top-full border rounded p-3 bg-white w-full'>
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 };
