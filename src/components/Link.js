05a47426882c09c4543de52d3ffee9978913de57
ClaytonDewey <clay@dryadmedia.com>
2024-04-20 14:16:34 -0500

    Setup route & navigation
    fix typos in pages files

---
+++ src/components/Link.js
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+import useNavigation from '../hooks/useNavigation';
+
+const Link = ({ to, children }) => {
+  const { navigate } = useNavigation();
+
+  const classes = classNames('text-blue-500');
+
+  const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) return;
+
+    event.preventDefault();
+    navigate(to);
+  };
+
+  return (
+    <a className={classes} onClick={handleClick} href={to}>
+      {children}
+    </a>
+  );
+};
+export default Link;
--- src/components/pages/DropdownPage.js
+++ src/components/pages/DropdownPage.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Dropdown from '../components/Dropdown';
+import Dropdown from '../Dropdown';
 
 const DropdownPage = () => {
   const [selection, setSelection] = useState(null);
